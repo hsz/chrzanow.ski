cea85211eafa19a2b25d05b0820d5cafdc02cd72
hsz <jakub@chrzanowski.info>
2020-07-07 07:32:16 +0000

    Deploying to gh-pages from  @ 2b06ff5cbb693ab94b2289daba786b76e2edd303 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ace58445ff50b2c475841d81c6e7371f.js"
+  "/precache-manifest.ca610e9f978991aafcea4b4de776b006.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
